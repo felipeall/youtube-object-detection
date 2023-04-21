d3c8489425eeba5813a94dddd1e00ea490b69a51
Felipe Allegretti <felipeallh@gmail.com>
2023-04-21 19:12:26 -0300

    Set GCP deploy on release

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,45 +13,6 @@ provider "google" {
   zone    = var.zone
 }
 
-# Enable IAM API
-resource "google_project_service" "iam" {
-  provider           = google
-  service            = "iam.googleapis.com"
-  disable_on_destroy = false
-}
-
-# Enable Artifact Registry API
-resource "google_project_service" "artifactregistry" {
-  provider           = google
-  service            = "artifactregistry.googleapis.com"
-  disable_on_destroy = false
-}
-
-# Enable Cloud Run API
-resource "google_project_service" "cloudrun" {
-  provider           = google
-  service            = "run.googleapis.com"
-  disable_on_destroy = false
-}
-
-# Enable Cloud Resource Manager API
-resource "google_project_service" "resourcemanager" {
-  provider           = google
-  service            = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
-
-# This is used so there is some time for the activation of the APIs to propagate through
-# Google Cloud before actually calling them.
-resource "time_sleep" "wait_30_seconds" {
-  create_duration = "30s"
-  depends_on = [
-    google_project_service.artifactregistry,
-    google_project_service.cloudrun,
-    google_project_service.resourcemanager
-  ]
-}
-
 # Deploy image to Cloud Run
 resource "google_cloud_run_service" "youtube_object_detection_api" {
   provider = google
