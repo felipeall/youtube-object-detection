ea688497cd09e79dc87fd54358313639d60928dc
Felipe Allegretti <felipeallh@gmail.com>
2023-04-21 19:10:09 -0300

    Implement GCP GitHub actions (#3)

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,15 +8,10 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.infra_credentials)
   project = var.project_id
   region  = var.region
   zone    = var.zone
 }
-
-#############################################
-#               Enable API's                #
-#############################################
 
 # Enable IAM API
 resource "google_project_service" "iam" {
@@ -46,7 +41,7 @@ resource "google_project_service" "resourcemanager" {
   disable_on_destroy = false
 }
 
-# This is used so there is some time for the activation of the API's to propagate through 
+# This is used so there is some time for the activation of the APIs to propagate through
 # Google Cloud before actually calling them.
 resource "time_sleep" "wait_30_seconds" {
   create_duration = "30s"
@@ -57,53 +52,9 @@ resource "time_sleep" "wait_30_seconds" {
   ]
 }
 
-#############################################
-#    Google Artifact Registry Repository    #
-#############################################
-
-# Create Artifact Registry Repository for Docker containers
-resource "google_artifact_registry_repository" "my_docker_repo" {
-  provider = google
-
-  location      = var.region
-  repository_id = var.repository
-  description   = "My docker repository"
-  format        = "DOCKER"
-  depends_on    = [time_sleep.wait_30_seconds]
-}
-
-# Create a Service Account
-resource "google_service_account" "docker_pusher" {
-  provider = google
-
-  account_id   = "docker-pusher"
-  display_name = "Docker Container Pusher"
-  depends_on   = [time_sleep.wait_30_seconds]
-}
-
-# Give Service Account permission to push to the Artifact Registry Repository
-resource "google_artifact_registry_repository_iam_member" "docker_pusher_iam" {
-  provider = google
-
-  location   = google_artifact_registry_repository.my_docker_repo.location
-  repository = google_artifact_registry_repository.my_docker_repo.repository_id
-  role       = "roles/artifactregistry.writer"
-  member     = "serviceAccount:${google_service_account.docker_pusher.email}"
-  depends_on = [
-    google_artifact_registry_repository.my_docker_repo,
-    google_service_account.docker_pusher
-  ]
-}
-
-##############################################
-#       Deploy API to Google Cloud Run       #
-#            (first_time = False)            #
-##############################################
-
 # Deploy image to Cloud Run
 resource "google_cloud_run_service" "youtube_object_detection_api" {
   provider = google
-  count    = var.first_time ? 0 : 1
   name     = "youtube-object-detection-api"
   location = var.region
   template {
@@ -129,13 +80,11 @@ resource "google_cloud_run_service" "youtube_object_detection_api" {
     percent         = 100
     latest_revision = true
   }
-  depends_on = [google_artifact_registry_repository_iam_member.docker_pusher_iam]
 }
 
 # Create a policy that allows all users to invoke the API
 data "google_iam_policy" "noauth" {
   provider = google
-  count    = var.first_time ? 0 : 1
   binding {
     role = "roles/run.invoker"
     members = [
@@ -146,15 +95,14 @@ data "google_iam_policy" "noauth" {
 
 # Apply the no-authentication policy to our Cloud Run Service.
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  count    = var.first_time ? 0 : 1
   provider = google
   location = var.region
   project  = var.project_id
-  service  = google_cloud_run_service.youtube_object_detection_api[0].name
+  service  = google_cloud_run_service.youtube_object_detection_api.name
 
-  policy_data = data.google_iam_policy.noauth[0].policy_data
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
 
 output "cloud_run_instance_url" {
-  value = var.first_time ? null : google_cloud_run_service.youtube_object_detection_api[0].status.0.url
+  value = google_cloud_run_service.youtube_object_detection_api.status.0.url
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "infra_credentials" {
-  description = "The file with GCP Service Account infra credentials"
-  type        = string
-  default     = "infra_service_account.json"
-}
-
 variable "project_id" {
   description = "The name of the project"
   type        = string
@@ -32,10 +26,4 @@ variable "docker_image" {
   description = "The name of the Docker image in the Artifact Registry repository to be deployed to Cloud Run"
   type        = string
   default     = "youtube-object-detection-api"
-}
-
-variable "first_time" {
-  description = "Boolean flag to indicate if this is the first time the application is running"
-  type        = bool
-  default     = true
 }
